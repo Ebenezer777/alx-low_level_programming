299c58ea873537f1630b1be198ac25302ee153f7
Ebenezer Assefa <assefaebenezer7@gmail.com>
2024-01-11 13:39:59 -0800

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@
 void times_table(void)
 
 {
-	int x, y, z, u,d;
+	int x, y, z, u, d;
 
 	for (x = 0; x <= 9; x++)
 	{
@@ -18,10 +18,10 @@ void times_table(void)
 	if (z > 9)
 	{
 	u = z % 10;
-	d = (z-u) / 10;
+	d = (z - u) / 10;
 	_putchar(44);
 	_putchar(32);
-	_putchar(d + 'a');
+	_putchar(d + '0');
 	_putchar(u + '0');
 	}
 	else
