9dee61fe967bd027613358e59e8fcc7bb20040b5
Ebenezer Assefa <assefaebenezer7@gmail.com>
2024-01-22 15:10:57 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - initializes a variable of type struct dog
+ *
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initializes
+ *
+ * Desc: first struct with alx
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
