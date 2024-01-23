3aa3ca57cd6d1787b0bd7dbe5624a2e770ddc4f1
Ebenezer Assefa <assefaebenezer7@gmail.com>
2024-01-22 16:05:38 -0800

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,19 @@
  * Desc: first struct with alx
  */
 
+/**
+ * dog_t - new name for struct dog
+ */
+
+typedef struct dog dog_t;
+
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
