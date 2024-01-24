068ae83380e97e45a573955c2b233016c0ce4eb6
Ebenezer Assefa <assefaebenezer7@gmail.com>
2024-01-23 17:23:36 -0800

    g

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,41 +14,41 @@ char *_strcpy(char *dest, char *src);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *max_dog;
-        int name_l = 0, own_l = 0;
+	dog_t *max_dog;
+	int name_l = 0, own_l = 0;
 
-        if (name != NULL && owner != NULL)
-        {
-                name_l = _strlen(name) + 1;
-                own_l = _strlen(owner) + 1;
-                max_dog = malloc(sizeof(dog_t));
+	if (name != NULL && owner != NULL)
+	{
+		name_l = _strlen(name) + 1;
+		own_l = _strlen(owner) + 1;
+		max_dog = malloc(sizeof(dog_t));
 
-                if (max_dog == NULL)
-                        return (NULL);
+		if (max_dog == NULL)
+			return (NULL);
 
-                max_dog->name = malloc(sizeof(char) * name_l);
+		max_dog->name = malloc(sizeof(char) * name_l);
 
-                if (max_dog->name == NULL)
-                {
-                        free(max_dog);
-                        return(NULL);
-                }
+		if (max_dog->name == NULL)
+		{
+			free(max_dog);
+			return (NULL);
+		}
 
-                max_dog->owner = malloc(sizeof(char) *own_l);
+		max_dog->owner = malloc(sizeof(char) *own_l);
 
-                if (max_dog->owner == NULL)
-                {
-                        free(max_dog->name);
-                        free(max_dog);
-                        return (NULL);
-                }
+		if (max_dog->owner == NULL)
+		{
+			free(max_dog->name);
+			free(max_dog);
+			return (NULL);
+		}
 
-                max_dog->name = _strcpy(max_dog->name, name);
-                max_dog->owner = _strcpy(max_dog->owner, owner);
-                max_dog->age = age;
-        }
+		max_dog->name = _strcpy(max_dog->name, name);
+		max_dog->owner = _strcpy(max_dog->owner, owner);
+		max_dog->age = age;
+		}
 
-        return (max_dog);
+		return (max_dog);
 }
 
 /**
@@ -85,7 +85,7 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 	}
 
-	dest [i++] = '\0';
+	dest[i++] = '\0';
 
 	return (dest);
 }
