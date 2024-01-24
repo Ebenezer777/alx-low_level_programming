ba1a0a156f00f2c7e70be90f5c10edd246a8c34a
Ebenezer Assefa <assefaebenezer7@gmail.com>
2024-01-23 17:11:30 -0800

    g

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,91 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 /**
  * new_dog - new dog to be created
  * @name: name of the new dog
  * @age: age of the new dog
  * @owner: owner of the new dog
+ *
  * Return: pointer to the new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	size_t n, o, i;
-	dog_t *dog;
+        dog_t *max_dog;
+        int name_l = 0, own_l = 0;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	for (n = 0; name[0] != '\0'; n++)
-		;
-	n++;
-	dog->name = malloc(sizeof(char) * n);
-	if (dog->name == NULL)
-	{
-		free(dog->name);
-		return (NULL);
-	}
-	for (i = 0; i < n; i++)
-		dog->name[i] = name[i];
-	dog->age = age;
-	for (o = 0; owner[0] != '\0'; o++)
-		;
-	o++;
-	dog->owner = malloc(sizeof(char) * o);
-	if (dog->owner == NULL)
-	{
-		free(dog->owner);
-		free(dog);
-		return (NULL);
-	}
-	for (i = 0; i < o; i++)
-		dog->owner[i] = owner[i];
-	return (dog);
+        if (name != NULL && owner != NULL)
+        {
+                name_l = _strlen(name) + 1;
+                own_l = _strlen(owner) + 1;
+                max_dog = malloc(sizeof(dog_t));
 
+                if (max_dog == NULL)
+                        return (NULL);
+
+                max_dog->name = malloc(sizeof(char) * name_l);
+
+                if (max_dog->name == NULL)
+                {
+                        free(max_dog);
+                        return(NULL);
+                }
+
+                max_dog->owner = malloc(sizeof(char) *own_l);
+
+                if (max_dog->owner == NULL)
+                {
+                        free(max_dog->name);
+                        free(max_dog);
+                        return (NULL);
+                }
+
+                max_dog->name = _strcpy(max_dog->name, name);
+                max_dog->owner = _strcpy(max_dog->owner, owner);
+                max_dog->age = age;
+        }
+
+        return (max_dog);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String Length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
+
+/**
+ * _strcpy - Copy a string
+ * @dest: Destination
+ * @src: Source value
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest [i++] = '\0';
+
+	return (dest);
 }
